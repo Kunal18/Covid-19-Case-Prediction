cace73959d7d494736845c4836d8943017a32749
KUNAL INDORE <kunalindore18@gmail.com>
2021-12-12 21:13:56 +0530

    Create import.py

---
+++ import.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+from fbprophet import Prophet
+from sklearn.metrics import r2_score
+
+plt.style.use("ggplot")
+
