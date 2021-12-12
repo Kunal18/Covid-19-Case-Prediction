09a793c492f9317019b45e01704127f97875459e
KUNAL INDORE <kunalindore18@gmail.com>
2021-12-12 21:17:14 +0530

    Update dataPrep.py

--- dataPrep.py
+++ dataPrep.py
@@ -27,3 +27,27 @@ world = world.groupby("Country")["Cases"].sum().reset_index()
 world.head()
 continent=pd.read_csv("../input/covidglobal/continents2.csv")
 continent["name"]=continent["name"].str.upper()
+
+
+world["Cases Range"]=pd.cut(world["Cases"],[-150000,50000,200000,800000,1500000,15000000],labels=["U50K","50Kto200K","200Kto800K","800Kto1.5M","1.5M+"])
+alpha =[]
+for i in world["Country"].str.upper().values:
+    if i == "BRUNEI":
+        i="BRUNEI DARUSSALAM"
+    elif  i=="US":
+        i="UNITED STATES" 
+    if len(continent[continent["name"]==i]["alpha-3"].values)==0:
+        alpha.append(np.nan)
+    else:
+        alpha.append(continent[continent["name"]==i]["alpha-3"].values[0])
+world["Alpha3"]=alpha
+
+fig = px.choropleth(world.dropna(),
+                   locations="Alpha3",
+                   color="Cases Range",
+                    projection="mercator",
+                    color_discrete_sequence=["white","khaki","yellow","orange","red"])
+fig.update_geos(fitbounds="locations", visible=False)
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
+
